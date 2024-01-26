4313a8b6b0efac052be11ab2d78a6a54a46314b0
DatVo1305 <125366123+DatVo1305@users.noreply.github.com>
2024-01-26 21:58:59 +0700

    Make conflict

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,11 @@
 ﻿class MyClass
 {
-    int a = 1;
-    int b = 2;
-    int c = 3;
-   
+    static int a = 100;
+    int b = 200;
+    int c = 300;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(a);
+    }
 }
